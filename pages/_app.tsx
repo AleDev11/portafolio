2909b93cedfe9d59d4848b4dd9b7d2b794dcd3b4
Alejandro Font <fontalejandro0@gmail.com>
2023-06-11 20:46:54 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <title>Alejandro Font</title>
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
